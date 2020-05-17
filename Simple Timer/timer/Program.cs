d2ee887664825054692e49a908986ff1943b73fa
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-05-17 21:29:12 +0300

    git add

---
+++ Simple Timer/timer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace testw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+
+            Console.Write("Введiть годину в форматi (година:хвилина:секунда): ");
+            var UserTimerTime = Console.ReadLine().Split(':').Select(item => Int32.Parse(item)).ToArray();
+
+            timer.GetTimeForTimer(UserTimerTime[0], UserTimerTime[1], UserTimerTime[2]);
+
+            timer.WaitForAlarm();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Simple Timer/timer/Timer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace testw
+{
+    class Timer
+    {
+        private int Hours { get; set; }
+        private int Minutes { get; set; }
+        private int Seconds { get; set; }
+        public Timer() { }
+        public Timer(int Hours, int Minutes, int Seconds)
+        {
+            if (Hours >=0 && Hours < 24)
+                this.Hours = Hours;
+            else
+                Console.WriteLine("Невiрно введений формат години!");
+
+            if (Minutes >=0 && Minutes < 60)
+                this.Minutes = Minutes;
+            else
+                Console.WriteLine("Невiрно введений формат хвилини!");
+
+            if (Seconds >= 0 && Seconds < 60)
+                this.Seconds = Seconds;
+            else
+                Console.WriteLine("Невiрно введений формат секунди!");
+        }
+        public override string ToString()
+        {
+            return $"{Hours}:{Minutes}:{Seconds}";
+        }
+        //Передача конкретної години, хвилини, секунди для таймеру
+        public void GetTimeForTimer(int Hours, int Minutes, int Seconds)
+        {
+            if (Hours >= 0 && Hours < 24)
+                this.Hours = Hours;
+            else
+                Console.WriteLine("Невiрно введений формат години!");
+
+            if (Minutes >= 0 && Minutes < 60)
+                this.Minutes = Minutes;
+            else
+                Console.WriteLine("Невiрно введений формат хвилини!");
+
+            if (Seconds >= 0 && Seconds < 60)
+                this.Seconds = Seconds;
+            else
+                Console.WriteLine("Невiрно введений формат секунди!");
+        }
+        //Таймер
+        public void WaitForAlarm()
+        {
+            while (true)
+                if (CheckForPresentTimeIsUserTime())
+                {
+                    GetAlarm();
+                    break;
+                }
+
+        }
+        //Метод який виконує перевірку на задану годину
+        private bool CheckForPresentTimeIsUserTime()
+        {
+            if (Hours == DateTime.Now.Hour && Minutes == DateTime.Now.Minute && Seconds == DateTime.Now.Second)
+                return true;
+            else
+                return false;
+        }
+        //Метод який виконує симуляцію сигналу протягом 1 хвилини
+        private void GetAlarm()
+        {
+            Console.WriteLine("Alarm! Alarm! Alarm! Alarm! Alarm! Alarm!");
+            for (int i = 0; i < 120; ++i)
+            {
+                Console.Beep();
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+    }
+}
