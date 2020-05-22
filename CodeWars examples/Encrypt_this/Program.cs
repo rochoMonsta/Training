c04af9a04dbc3be79c46a0d74dbfbeb94ee900c0
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-05-22 16:50:25 +0300

    git add

---
+++ CodeWars examples/Encrypt_this/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypt_this
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(EncryptThis("hello world"));
+            Console.ReadLine();
+        }
+        //public static string EncryptThis(string input)
+        //{
+        //    string result_input_after_encrypt = "";
+
+        //    if (input == "")
+        //        return result_input_after_encrypt;
+        //    else
+        //    {
+        //        var words_array = input.Split();
+        //        for (int i = 0; i < words_array.Length; ++i)
+        //        {
+        //            if (words_array[i].Length == 1)
+        //            {
+        //                result_input_after_encrypt += (int)words_array[i][0];
+        //                if (i != words_array.Length - 1)
+        //                    result_input_after_encrypt += ' ';
+        //            }
+        //            if (words_array[i].Length == 2)
+        //            {
+        //                result_input_after_encrypt += (int)words_array[i][0];
+        //                result_input_after_encrypt += words_array[i][1];
+        //                if (i != words_array.Length - 1)
+        //                    result_input_after_encrypt += ' ';
+        //            }
+        //            if (words_array[i].Length >= 3)
+        //            {
+        //                result_input_after_encrypt += (int)words_array[i][0];
+        //                var word_char_array = words_array[i].ToArray();
+
+        //                char char_box = word_char_array[1];
+        //                word_char_array[1] = word_char_array[word_char_array.Length - 1];
+        //                word_char_array[word_char_array.Length - 1] = char_box;
+
+        //                for (int j = 1; j < word_char_array.Length; ++j)
+        //                    result_input_after_encrypt += word_char_array[j];
+        //                if (i != words_array.Length - 1)
+        //                    result_input_after_encrypt += ' ';
+        //            }
+        //        }
+        //        return result_input_after_encrypt;
+        //    }
+        //}
+        //public static string EncryptThis(string s)
+        //  => string.Join(" ", s.Split(' ').Select(Encrypt));
+        //public static string Encrypt(string input)
+        //{
+        //    if (string.IsNullOrEmpty(input)) return "";
+        //    else if (input.Length == 1) return $"{(int)input[0]}";
+        //    else if (input.Length == 2) return $"{(int)input[0]}{input[1]}";
+        //    return $"{(int)input[0]}{input.Last()}{input.Substring(2, input.Length - 3)}{input[1]}";
+        //}
+        public static string EncryptThis(string input)
+        {
+            if (input.Length < 1) return "";
+            string rtn = "";
+            foreach (string s in input.Split())
+            {
+                rtn += (int)(s[0]);
+                if (s.Length > 1) rtn += s[s.Length - 1];
+                if (s.Length > 3) rtn += s.Substring(2, s.Length - 3);
+                if (s.Length > 2) rtn += s[1];
+                rtn += " ";
+            }
+            return rtn.Trim();
+        }
+    }
+}
---
+++ CodeWars examples/JosephusPermutation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JosephusPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> list = new List<object>() { 1, 2, 3, 4, 5, 6, 7 };
+            PrintList(JosephusPermutation(list, 3));
+            Console.ReadLine();
+        }
+        //public static List<object> JosephusPermutation(List<object> items, int k)
+        //{
+        //    List<object> list = new List<object>();
+        //    if (items.Count > 0)
+        //    {
+        //        int element_index = k - 1;
+        //        while (items.Count != 1)
+        //        {
+        //            if (element_index % items.Count == 0)
+        //                element_index = 0;
+        //            else
+        //                element_index = element_index > items.Count ? element_index % items.Count : element_index;
+        //            list.Add(items[element_index]);
+        //            items.RemoveAt(element_index);
+        //            element_index += k - 1;
+        //        }
+        //        list.Add(items[items.Count - 1]);
+        //        return list;
+        //    }
+        //    else
+        //        return items;
+        //}
+        public static List<object> JosephusPermutation(List<object> items, int k)
+        {
+            int index = 0;
+            List<object> list = new List<object>();
+            while (items.Count > 0)
+            {
+                index = (index + k - 1) % items.Count;
+                list.Add(items[index]);
+                items.RemoveAt(index);
+            }
+            return list;
+        }
+        public static void PrintList <T> (List<T> list)
+        {
+            foreach (T element in list)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+    }
+}
