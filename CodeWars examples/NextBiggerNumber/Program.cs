41fa67a055db4e4f2bf54970b6c42627519fcc9a
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-05-24 16:53:11 +0300

    git add

---
+++ CodeWars examples/NextBiggerNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NextBiggerNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NextBiggerNumber(2017));
+            Console.ReadLine();
+        }
+        public static long NextBiggerNumber(long n)
+        {
+            long result;
+            var current_number_array = n.ToString().Select(number => (int)char.GetNumericValue(number)).ToList();
+            for (int i = current_number_array.Count - 1; i > 0; --i)
+            {
+                if (current_number_array[i] > current_number_array[i - 1])
+                {
+                    int box = current_number_array[i];
+                    current_number_array[i] = current_number_array[i - 1];
+                    current_number_array[i - 1] = box;
+
+                    result = GetNumber(current_number_array);
+                    if (n < result)
+                        return result;
+                    else
+                        continue;
+                }
+            }
+            return -1;
+        }
+        public static long GetNumber <T> (List<T> list)
+        {
+            string result = "";
+            foreach (T element in list)
+                result += element;
+            return Int32.Parse(result);
+        }
+    }
+}
---
+++ CodeWars examples/NextBiggerNumber/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("NextBiggerNumber")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NextBiggerNumber")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f1c62afa-023b-469c-9937-048c05b0320b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
