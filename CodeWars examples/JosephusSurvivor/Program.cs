2742f225e390b9ad003608068ff14e2f247fa47e
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-05-26 21:56:13 +0300

    git add

---
+++ CodeWars examples/JosephusSurvivor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JosephusSurvivor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(JosSurvivor(11, 19));
+            Console.ReadLine();
+        }
+        public static int JosSurvivor(int n, int k)
+        {
+            int person_index = 0;
+            List<int> people_numbe_list = new List<int>();
+            for (int i = 1; i <= n; ++i)
+                people_numbe_list.Add(i);
+            while (people_numbe_list.Count > 1)
+            {
+                person_index = (person_index + k - 1) % people_numbe_list.Count;
+                people_numbe_list.RemoveAt(person_index);
+            }
+            return people_numbe_list[0];
+        }
+    }
+}
