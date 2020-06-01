7fd067c545412a5390b689290af901bee5cb2185
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-01 10:30:06 +0300

    git add

---
+++ CodeWars examples/Sum_of_Digits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sum_of_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DigitalRoot(493193));
+            Console.ReadLine();
+        }
+        public static int DigitalRoot(long n)
+        {
+            var input = n.ToString();
+            while (input.Length != 1)
+            {
+                int result = 0;
+                for (int i = 0; i < input.Length; ++i)
+                    result += Int32.Parse(input[i].ToString());
+                input = result.ToString();
+            }
+            return Int32.Parse(input);
+        }
+    }
+}
