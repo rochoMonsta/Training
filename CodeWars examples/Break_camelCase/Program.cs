cd5bacf3b4ec5354edc9abaeab861aceb482f7bf
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-02 00:43:36 +0300

    git add

---
+++ CodeWars examples/Break_camelCase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Break_camelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BreakCamelCase("camelCasingTest"));
+            Console.ReadLine();
+        }
+        public static string BreakCamelCase(string str)
+        {
+            string result_string = "";
+            for (int i = 0; i < str.Length; ++i)
+            {
+                if (!Char.IsUpper(str[i]))
+                    result_string += str[i];
+                else
+                    result_string = result_string + ' ' + str[i];
+            }
+            return result_string;
+        }
+    }
+}
