b5cba57c618f18f4f9f1358fffbeec85fe2f2af5
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-02 00:53:38 +0300

    git add

---
+++ CodeWars examples/Onion_array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Onion_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsOnionArray(new int[] { 1, 1, 15, 10, -1 }));
+            Console.ReadLine();
+        }
+        public static bool IsOnionArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length / 2; ++i)
+            {
+                if (!(arr[i] + arr[arr.Length - 1 - i] <= 10))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
