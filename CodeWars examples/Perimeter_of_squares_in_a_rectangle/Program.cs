fc8fb2513fd1935d38c213b64af52ea08bd2eede
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-04 17:45:42 +0300

    git add

---
+++ CodeWars examples/Perimeter_of_squares_in_a_rectangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+namespace Perimeter_of_squares_in_a_rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(perimeter(5));
+            Console.ReadLine();
+        }
+        public static BigInteger perimeter(BigInteger squares_count)
+        {
+            BigInteger number1 = 1, number2 = 1, result = 2;
+            for (int i = 2; i <= squares_count; ++i)
+            {
+                result += number1 + number2;
+                var number_box = number2;
+                number2 += number1;
+                number1 = number_box;
+            }
+            return 4 * result;
+        }
+    }
+}
---
+++ CodeWars examples/Perimeter_of_squares_in_a_rectangle/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Perimeter_of_squares_in_a_rectangle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Perimeter_of_squares_in_a_rectangle")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d36f1a75-301d-4ef7-aa64-41f7cac2c02d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
