baeeafdc98c28d2a937d1449382fba8a3421f155
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-05 23:33:15 +0300

    git add

---
+++ CodeWars examples/Are_they_the_same/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Are_they_the_same
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = { 121, 14641, 20736, 361, 25921, 361, 20736 };
+            Console.WriteLine(comp(a, b));
+            Console.ReadLine();
+        }
+        public static bool comp(int[] a, int[] b)
+        {
+            if (a == null || b == null || a.Length != b.Length)
+                return false;
+            else
+            {
+                var bList = b.ToList();
+                foreach (var number in a)
+                {
+                    if (!bList.Contains(number * number))
+                        return false;
+                    else
+                        bList.Remove(number * number);
+                }
+                return true;
+            }
+        }
+    }
+}
