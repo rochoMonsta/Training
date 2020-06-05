91a4ecefd00e96948b8cfb3312d0cecf32033133
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-05 21:40:16 +0300

    git add

---
+++ CodeWars examples/String_Letter_Counting/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace String_Letter_Counting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringLetterCount("This is a test sentence."));
+            Console.WriteLine(StringLetterCount("The quick brown fox jumps over the lazy dog."));
+            Console.WriteLine(StringLetterCount("The time you enjoy wasting is not wasted time."));
+            Console.ReadLine();
+        }
+        //Description:
+        //Take an input string and return a string that is made up of the number of occurances of each english letter
+        //in the input, followed by that letter. The string shouldn't contain zeros; leave them out.
+
+        //An empty string, or one with no letters, should return an empty string.
+
+        //Notes:
+        //the input will always be valid;
+        //treat letters as case-insensitive
+        public static string StringLetterCount(string sentence)
+        {
+            string result = "";
+            sentence = sentence.ToLower();
+            Dictionary<char, int> letters_dict = new Dictionary<char, int>();
+            foreach (var letter in sentence)
+            {
+                List<char> key_list = new List<char>(letters_dict.Keys);
+                if (Char.IsLetter(letter))
+                {
+                    if (key_list.Contains(letter))
+                        letters_dict[letter]++;
+                    else
+                        letters_dict.Add(letter, 1);
+                }
+            }
+            var order_collection = letters_dict.OrderBy(element => element.Key);
+            foreach (var element in order_collection)
+                result += element.Value.ToString() + element.Key.ToString();
+            return result;
+        }
+    }
+}
---
+++ CodeWars examples/String_Letter_Counting/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("String_Letter_Counting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("String_Letter_Counting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d780e715-0c6d-4ab4-b214-9c93b0f9637d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
