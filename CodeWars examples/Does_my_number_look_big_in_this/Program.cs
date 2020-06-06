d3e73a247250a170615b6e2b89342cc8e2a62105
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-06 19:52:39 +0300

    git add

---
+++ CodeWars examples/Does_my_number_look_big_in_this/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Does_my_number_look_big_in_this
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Narcissistic(1634));
+            Console.ReadLine();
+        }
+        //A Narcissistic Number is a number which is the sum of its own digits, 
+        //each raised to the power of the number of digits in a given base. 
+        //In this Kata, we will restrict ourselves to decimal (base 10).
+        public static bool Narcissistic(int value)
+        {
+            double result = 0;
+            var valueAsString = value.ToString();
+            foreach (var element in valueAsString)
+                result += Math.Pow(Char.GetNumericValue(element), valueAsString.Length);
+            if (result == value)
+                return true;
+            return false;
+        }
+        //public static bool Narcissistic(int value)
+        //{
+        //    var str = value.ToString();
+        //    return str.Sum(c => Math.Pow(Convert.ToInt16(c.ToString()), str.Length)) == value;
+        //}
+    }
+}
---
+++ CodeWars examples/Does_my_number_look_big_in_this/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Does_my_number_look_big_in_this")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Does_my_number_look_big_in_this")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a1e057fe-1054-4e6c-88b8-627f509c3a45")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
