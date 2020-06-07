fbeecb0159b1099f33a82c2f65111a9d2ad608b6
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-07 19:43:37 +0300

    git add

---
+++ CodeWars examples/Duplicate_Encoder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Duplicate_Encoder
+{
+    class Program
+    {
+        //The goal of this exercise is to convert a string to a new string where each character in the 
+        //new string is "(" if that character appears only once in the original string, or ")" if that 
+        //character appears more than once in the original string. Ignore capitalization when determining 
+        //if a character is a duplicate.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DuplicateEncode("Success"));
+            Console.ReadLine();
+        }
+        public static string DuplicateEncode(string word)
+        {
+            string result = "";
+            word = word.ToLower();
+            foreach (var letter in word)
+            {
+                if (word.Count(x => x == letter) >= 2)
+                    result += ')';
+                else
+                    result += '(';
+            }
+            return result;
+        }
+        //public static string DuplicateEncode(string word)
+        //{
+        //    return new string(word.ToLower().Select(ch => word.ToLower().Count(innerCh => ch == innerCh) == 1 ? '(' : ')').ToArray());
+        //}
+    }
+}
---
+++ CodeWars examples/Duplicate_Encoder/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Duplicate_Encoder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Duplicate_Encoder")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5a030207-8f65-4d48-aef4-5f7e1e62ea4d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
