5e6b387df8a2ac6eec94c4eb35eb920456ec27cf
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-08 00:23:34 +0300

    git add

---
+++ CodeWars examples/Consonant_value/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Consonant_value
+{
+    class Program
+    {
+        //private static readonly char[] Vowels = { 'a', 'e', 'i', 'o', 'u' };
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("mischtschenkoana"));
+            Console.ReadLine();
+        }
+        
+        public static int Solve(string s)
+        {
+            string result = "";
+            char[] consonants = { 'a', 'e', 'i', 'o', 'u' };
+            foreach (var letter in s)
+            {
+                if (!consonants.Contains(letter))
+                    result += letter;
+                else
+                    result += ' ';
+            }
+            var vowels_letters = Regex.Replace(result, "[ ]+", " ").Split();
+            return GetHighestVowelIndex(vowels_letters);
+        }
+        public static int GetHighestVowelIndex(string[] vowels)
+        {
+            int max = 0;
+            for (int i = 0; i < vowels.Length; ++i)
+            {
+                int local_max = 0;
+                foreach (var letter in vowels[i])
+                    local_max += (int)letter % 32;
+                if (local_max > max)
+                    max = local_max;
+            }
+            return max;
+        }
+        //public static int Solve(string input)
+        //{
+        //    return input.ToLowerInvariant()
+        //      .Split(Vowels, StringSplitOptions.RemoveEmptyEntries)
+        //      .Max(s => s.Sum(c => c - 'a' + 1));
+        //}
+    }
+}
---
+++ CodeWars examples/Consonant_value/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Consonant_value")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Consonant_value")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("bc70a08d-a693-4e9f-b55e-8b35e00bc901")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
