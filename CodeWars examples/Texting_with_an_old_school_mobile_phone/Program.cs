43d5cbee8e0b767cffa7690627ececb121f11f8c
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-11 20:32:06 +0300

    git add

---
+++ CodeWars examples/Texting_with_an_old_school_mobile_phone/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Texting_with_an_old_school_mobile_phone
+{
+    class Program
+    {
+        public static Dictionary<string, string> phone_buttons = new Dictionary<string, string>()
+        {
+            {"1", ".,?!" }, {"2", "abc"}, {"3", "def"},
+            {"4", "ghi" },  {"5", "jkl"}, {"6", "mno"},
+            {"7", "pqrs" }, {"8", "tuv"}, {"9", "wxyz"},
+            {"*", "'-+=" }, {"0", " "}, {"0-", "0"},
+            {"1-", "1"}, {"2-", "2"}, {"3-", "3"},
+            {"4-", "4"}, {"5-", "5"}, {"6-", "6"},
+            {"7-", "7"}, {"8-", "8"}, {"9-", "9"},
+            {"*-", "*"}, {"#-", "#"}
+        };
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SendMessage(".,?! '-+=*#"));
+            Console.ReadLine();
+        }
+        public static string SendMessage(string message)
+        {
+            string result = " ";
+            bool isUpper = false;
+            var value_list = new List<string>(phone_buttons.Values);
+            foreach (var letter in message)
+            {
+                char letter_copy = letter;
+                if (Char.IsUpper(letter_copy) && isUpper == false)
+                {
+                    result += '#';
+                    letter_copy = Char.ToLower(letter_copy);
+                    isUpper = true;
+                }
+                else if (Char.IsUpper(letter_copy) && isUpper == true)
+                {
+                    letter_copy = Char.ToLower(letter_copy);
+                    isUpper = true;
+                }
+                else if (isUpper && Char.IsLower(letter_copy))
+                {
+                    result += '#';
+                    isUpper = false;
+                }
+                for (int i = 0; i < value_list.Count; ++i)
+                {
+                    if (value_list[i].Contains(letter_copy))
+                    {
+                        if (result[result.Length - 1] != phone_buttons.ElementAt(i).Key[0])
+                        {
+                            for (int j = 0; j < value_list[i].IndexOf(letter_copy) + 1; ++j)
+                                result += phone_buttons.ElementAt(i).Key;
+                            break;
+                        }
+                        else
+                        {
+                            result += ' ';
+                            for (int j = 0; j < value_list[i].IndexOf(letter_copy) + 1; ++j)
+                                result += phone_buttons.ElementAt(i).Key;
+                            break;
+                        }
+                    }
+                }
+            }
+            return result.Substring(1, result.Length - 1);
+        }
+    }
+}
---
+++ CodeWars examples/Texting_with_an_old_school_mobile_phone/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Texting_with_an_old_school_mobile_phone")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Texting_with_an_old_school_mobile_phone")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2b25635f-5df0-4392-9a19-fef3dcdcd4b6")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
