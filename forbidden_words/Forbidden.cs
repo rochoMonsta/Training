ad69b8ebd08b417d5f988c6bd8865abc9046d187
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-13 15:13:25 +0300

    git add

---
+++ forbidden_words/Forbidden.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace forbidden_words
+{
+    class Forbidden
+    {
+        #region Fields
+        private List<string> forbidden_words_list = new List<string>()
+        {
+            "suck", "dick", "pussy", "nigga", "moron", "fuck", "noob"
+        };
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Replaces all forbidden words in the text with asterisks
+        /// </summary>
+        /// <param name="text" text entered by users></param>
+        /// <returns></returns>
+        public string CheckForForbiddenWord(string text)
+        {
+            text = text.ToLower();
+            foreach (var element in forbidden_words_list)
+                if (text.Contains(element))
+                    text = text.Replace(element, new string('*', element.Length));
+            return text;
+        }
+        #endregion
+    }
+}
---
+++ forbidden_words/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace forbidden_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forbidden forbidden = new Forbidden();
+            string user_message = "You fucking noob and puSSy";
+            Console.WriteLine(forbidden.CheckForForbiddenWord(user_message));
+            Console.ReadLine();
+        }
+    }
+}
