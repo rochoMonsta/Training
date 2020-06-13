fc243d5cbfb6c36b3354de5b927b1209bc2289a1
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-13 15:38:12 +0300

    git add

---
+++ CodeWars examples/Normalizing_Out_of_Range_Array_Indexes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Normalizing_Out_of_Range_Array_Indexes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NormIndex(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, -11));
+            Console.ReadLine();
+        }
+        public static int NormIndex(int[] array, int index)
+        {
+            if (index > 0)
+                return array[index % array.Length];
+            else
+            {
+                if (Math.Abs(index) % array.Length == 0)
+                    return array[0];
+                else
+                    return array[array.Length - (Math.Abs(index) % array.Length)];
+            }
+        }
+        //public static int NormIndex(int[] array, int index)
+        //{
+        //    while (index < 0) index += array.Length;
+        //    return array[index % array.Length];
+        //}
+    }
+}
