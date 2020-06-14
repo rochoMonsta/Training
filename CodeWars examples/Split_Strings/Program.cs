f9c7e8439e82c6fefd2419924f20f8a3320d773c
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-14 15:40:09 +0300

    git add

---
+++ CodeWars examples/Split_Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Split_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintArray(Solution("abcdefg"));
+            Console.ReadLine();
+        }
+        public static string[] Solution(string str)
+        {
+            List<string> lst = new List<string>();
+            for (int i = 0; i < str.Length - (str.Length % 2); i += 2)
+                lst.Add(str.Substring(i, 2));
+            if (str.Length % 2 != 0)
+                lst.Add(String.Concat(str[str.Length - 1], '_'));
+            return lst.ToArray();
+        }
+        public static void PrintArray <T> (T[] array)
+        {
+            foreach (var element in array)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+    }
+}
