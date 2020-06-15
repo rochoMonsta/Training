bafb2ac6262b8c6d25722a763c76830758005015
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-15 12:55:40 +0300

    git add

---
+++ CodeWars examples/Persistent_Bugger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Persistent_Bugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Persistence(999));
+            Console.ReadLine();
+        }
+        public static int Persistence(long n)
+        {
+            int counter = 0;
+            while (n.ToString().Length != 1)
+            {
+                int result = 1;
+                var n_as_string = n.ToString();
+                for (int i = 0; i < n_as_string.Length; ++i)
+                    result *= Int32.Parse(n_as_string[i].ToString());
+                n = result;
+                counter++;
+            }
+            return counter;
+        }
+    }
+}
