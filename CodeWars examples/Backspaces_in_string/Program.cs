0bb6f8f961ad0e52480a3857152dab53b9f60a07
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-16 13:29:20 +0300

    git add

---
+++ CodeWars examples/Backspaces_in_string/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Backspaces_in_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CleanString("abc####d##c#"));
+            Console.ReadLine();
+        }
+        public static string CleanString(string s)
+        {
+            string result = "";
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (s[i] != '#')
+                    result += s[i];
+                else
+                {
+                    if (result.Length > 0)
+                        result = result.Substring(0, result.Length - 1);
+                }
+            }
+            return result;
+        }
+    }
+}
