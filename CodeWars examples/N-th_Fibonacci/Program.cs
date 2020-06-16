378b080091db6f24f525e2835fa235ff88d685ae
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-17 00:45:50 +0300

    git add

---
+++ CodeWars examples/N-th_Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace N_th_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NthFib(2));
+            Console.ReadLine();
+        }
+        public static int NthFib(int n)
+        {
+            if (n == 1)
+                return 0;
+            else if (n == 2)
+                return 1;
+            else
+            {
+                int first = 0, second = 1;
+                for (int counter = 2; counter < n; ++counter)
+                {
+                    int box_var = second;
+                    second += first;
+                    first = box_var;
+                }
+                return second;
+            }
+        }
+    }
+}
---
+++ CodeWars examples/N-th_Fibonacci/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("N-th_Fibonacci")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("N-th_Fibonacci")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8dd41b1e-2151-4d32-8522-624d2665b1bb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
