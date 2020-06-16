d1fb1a1db5be3853c966be8b16e0033538043238
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-17 00:45:29 +0300

    git add

---
+++ CodeWars examples/Two_Joggers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Two_Joggers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = NbrOfLaps(4, 8);
+            Console.WriteLine(result.Item1 + " " + result.Item2);
+            Console.ReadLine();
+        }
+        public static Tuple<int, int> NbrOfLaps(int x, int y)
+        {
+            int max = x > y ? x : y;
+            while (max % x != 0 || max % y != 0)
+                max++;
+            return new Tuple<int, int>(max / x, max / y);
+        }
+    }
+}
