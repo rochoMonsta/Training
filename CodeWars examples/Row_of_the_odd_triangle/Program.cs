81af47d05921672b3c0d312ba9ce3523bcc4365e
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-19 23:25:35 +0300

    git add

---
+++ CodeWars examples/Row_of_the_odd_triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Row_of_the_odd_triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintArr(OddRow(4));
+            Console.ReadLine();
+        }
+        public static long[] OddRow(int n)
+        {
+            long x0 = (long)n * (n - 1) + 1;
+            return Enumerable.Range(0, n).Select(i => x0 + 2 * i).ToArray();
+        }
+        public static void PrintArr<T>(T[] array)
+        {
+            foreach (var element in array)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+        //public static long[] OddRow(int n)
+        //{
+        //    var odd_numbers_arr = new long[n];
+        //    long count_before = (long)n * (n - 1) + 1;
+        //    for (int i = 0; i < n; ++i)
+        //    {
+        //        odd_numbers_arr[i] = count_before;
+        //        count_before += 2;
+        //    }
+        //    return odd_numbers_arr;
+        //}
+    }
+}
