aba43e3b427d61ae87922c03691f2176d6db6853
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-20 19:13:00 +0300

    git add

---
+++ CodeWars examples/Stop_gninnipS_My_sdroW/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Stop_gninnipS_My_sdroW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SpinWords("You are almost to the last test"));
+            Console.ReadLine();
+        }
+        public static string SpinWords(string sentence)
+        {
+            var words_array = sentence.Split();
+            for (int i = 0; i < words_array.Length; ++i)
+            {
+                if (words_array[i].Length > 4)
+                    words_array[i] = new string(words_array[i].Reverse().ToArray());
+            }
+            return String.Join(" ", words_array);
+        }
+        //public static string SpinWords(string sentence)
+        //{
+        //    return String.Join(" ", sentence.Split().Select(word => word.Length >= 5 ? new string(word.Reverse().ToArray()) : word));
+        //}
+    }
+}
