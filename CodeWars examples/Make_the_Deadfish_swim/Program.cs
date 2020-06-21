f6dc39479c8f5848cc68e82728ebc4f99dfcbd9b
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-21 03:18:40 +0300

    git add

---
+++ CodeWars examples/Make_the_Deadfish_swim/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Make_the_Deadfish_swim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintArray(Parse("iiisdeeeosodddddiso"));
+            Console.ReadLine();
+        }
+        public static int[] Parse(string data)
+        {
+            int number = 0, counter = 0;
+            int[] output_array = new int[data.Count(x => x == 'o')];
+            foreach (var letter in data)
+            {
+                switch (letter)
+                {
+                    case 'i': number++; break;
+                    case 'd': number--; break;
+                    case 's': number *= number; break;
+                    case 'o': output_array[counter] = number; counter++; break;
+                    default: break;
+                }
+            }
+            return output_array;
+        }
+        public static void PrintArray <T> (T[] array)
+        {
+            foreach (var element in array)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+    }
+}
