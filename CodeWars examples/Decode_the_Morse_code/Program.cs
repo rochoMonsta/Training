97ca53b15b4ea52457304156358d25b5b29bcccd
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-22 01:23:00 +0300

    git add

---
+++ CodeWars examples/Decode_the_Morse_code/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Decode_the_Morse_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Decode("   .   "));
+            Console.ReadLine();
+        }
+        static readonly Regex trimmer = new Regex(@"\s\s+");
+        public static Dictionary<string, string> _morseAlphabetDictionary = new Dictionary<string, string>()
+        {
+            {"a", ".-"},
+            {"b", "-..."},
+            {"c", "-.-."},
+            {"d", "-.."},
+            {"e", "."},
+            {"f", "..-."},
+            {"g", "--."},
+            {"h", "...."},
+            {"i", ".."},
+            {"j", ".---"},
+            {"k", "-.-"},
+            {"l", ".-.."},
+            {"m", "--"},
+            {"n", "-."},
+            {"o", "---"},
+            {"p", ".--."},
+            {"q", "--.-"},
+            {"r", ".-."},
+            {"s", "..."},
+            {"t", "-"},
+            {"u", "..-"},
+            {"v", "...-"},
+            {"w", ".--"},
+            {"x", "-..-"},
+            {"y", "-.--"},
+            {"z", "--.."},
+            {"0", "-----"},
+            {"1", ".----"},
+            {"2", "..---"},
+            {"3", "...--"},
+            {"4", "....-"},
+            {"5", "....."},
+            {"6", "-...."},
+            {"7", "--..."},
+            {"8", "---.."},
+            {"9", "----."},
+            {"SOS", "...---..."},
+            {".", ".-.-.-"},
+            {",", "--..--"},
+            {"!", "-.-.--"},
+            {"?", "..--.."}
+        };
+        public static string Decode(string morseCode)
+        {
+            string sentences = "";
+            var morse_words_codes_ = morseCode.Split();
+            foreach (var word_code_ in morse_words_codes_)
+            {
+                if (String.IsNullOrWhiteSpace(word_code_))
+                    sentences += " ";
+                else
+                    sentences += _morseAlphabetDictionary.FirstOrDefault(x => x.Value == word_code_).Key;
+            }
+            return trimmer.Replace(sentences.ToUpper().Trim(), " ");
+        }
+    }
+}
---
+++ CodeWars examples/Decode_the_Morse_code/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Decode_the_Morse_code")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Decode_the_Morse_code")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0de44b05-cc0c-4499-8a40-38eb773160f8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
