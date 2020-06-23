49866b51c0118e5520dc6c50380b1613cacb509d
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-23 16:45:42 +0300

    git add

---
+++ CodeWars examples/Christmas_tree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Christmas_tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ChristmasTree(30));
+            Console.ReadLine();
+        }
+        public static string ChristmasTree(int height)
+        {
+            if (height == 0)
+                return "";
+            else
+            {
+                string tree = "";
+                for (int i = 0; i < height; ++i)
+                {
+                    tree += string.Concat(new string(' ', height - i - 1),
+                                         new string('*', i * 2 + 1),
+                                         new string(' ', height - i - 1), "\n");
+                }
+                return tree.Remove(tree.Length - 1);
+            }
+        }
+    }
+}
