c54563c9e00496a731f1778c009fb7a6402a6eec
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-23 16:23:17 +0300

    git add

---
+++ CodeWars examples/Multiples_of_3_or_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Multiples_of_3_or_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution(10));
+            Console.ReadLine();
+        }
+        public static int Solution(int value)
+        {
+            return Enumerable.Range(0, value).Where(i => i % 3 == 0 || i % 5 == 0).Sum();
+        }
+    }
+}
