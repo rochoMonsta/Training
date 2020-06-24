c4c0fbb777627d1d596461eed1a8ed7e169de861
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-24 14:48:41 +0300

    git add

---
+++ CodeWars examples/Take_a_Ten_Minute_Walk/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Take_a_Ten_Minute_Walk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValidWalk(new string[] { "n", "n", "n", "s", "n", "s", "n", "s", "n", "s" }));
+            Console.ReadLine();
+        }
+        public static bool IsValidWalk(string[] walk)
+        {
+            if (walk.Length != 10)
+                return false;
+            else
+            {
+                if (walk.Count(x => x == "w") == walk.Count(y => y == "e") &&
+                    walk.Count(x => x == "n") == walk.Count(x => x == "s"))
+                    return true;
+                else
+                    return false;
+            }
+        }
+    }
+}
---
+++ CodeWars examples/Take_a_Ten_Minute_Walk/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Take_a_Ten_Minute_Walk")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Take_a_Ten_Minute_Walk")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("89754c57-e453-4ac9-9348-f2f5a5be3528")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
