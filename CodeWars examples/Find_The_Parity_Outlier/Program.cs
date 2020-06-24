e6ab37a33d3d305c6d5988f6e88f3ecbfc9b467f
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-24 14:31:52 +0300

    git add

---
+++ CodeWars examples/Find_The_Parity_Outlier/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Find_The_Parity_Outlier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Find(new int[] { 160, 3, 1719, 19, 11, 13, -21 }));
+            Console.ReadLine();
+        }
+        public static int Find(int[] integers)
+        {
+            int result = integers.Count(x => x % 2 == 0) > 1 ?
+                         integers.FirstOrDefault(z => z % 2 != 0) : 
+                         integers.FirstOrDefault(k => k % 2 == 0);
+            return result;
+        }
+    }
+}
