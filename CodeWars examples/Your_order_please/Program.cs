067ba335bb608b00e0a8931a3518ab3213439634
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-28 16:31:19 +0300

    git add

---
+++ CodeWars examples/Your_order_please/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Your_order_please
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Order("is2 Thi1s T4est 3a"));
+            Console.ReadLine();
+        }
+        //public static string Order(string words)
+        //{
+        //    if (String.IsNullOrWhiteSpace(words))
+        //        return "";
+        //    else
+        //    {
+        //        string result = "";
+        //        var words_list = new List<Word>();
+        //        foreach (var element in words.Split())
+        //        {
+        //            var word = new Word();
+        //            word.word = element;
+        //            foreach (var letter in element)
+        //                if (Char.IsDigit(letter))
+        //                    word.word_position = Convert.ToInt32(letter.ToString());
+        //            words_list.Add(word);
+        //        }
+        //        words_list = words_list.OrderBy(x => x.word_position).ToList();
+        //        foreach (var element in words_list)
+        //            result += element.word + " ";
+        //        return result.Trim();
+        //    }
+        //}
+        public static string Order(string words)
+        {
+            if (string.IsNullOrEmpty(words))
+                return "";
+            else
+                return string.Join(" ", words.Split().OrderBy(word => word.ToList().Find(letter => char.IsDigit(letter))));
+        }
+    }
+    //public class Word
+    //{
+    //    public string word { get; set; }
+    //    public int word_position { get; set; }
+    //}
+}
