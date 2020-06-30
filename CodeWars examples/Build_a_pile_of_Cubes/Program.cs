3d7dd97ffb9457fb312b76a8df8830c98e4fdfff
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-07-01 00:02:42 +0300

    git add

---
+++ CodeWars examples/Build_a_pile_of_Cubes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Build_a_pile_of_Cubes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(findNb(135440716410000));
+            Console.ReadLine();
+        }
+        public static long findNb(long m)
+        {
+            int counter = 1;
+            long cube_volume = 0;
+            while (cube_volume < m)
+            {
+                cube_volume += Convert.ToInt64(Math.Pow(counter, 3));
+                counter++;
+            }
+            return cube_volume == m ? Convert.ToInt64(counter) - 1 : -1;
+        }
+    }
+}
