581b72eceb3de204b08715f3734a6e2f1c92871a
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-06-30 23:09:46 +0300

    git add

---
+++ CodeWars examples/Count_the_divisible_numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Count_the_divisible_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DivisibleCount(6, 11, 2));
+            Console.ReadLine();
+        }
+        public static long DivisibleCount(long x, long y, long k)
+        {
+            return Convert.ToInt64(Enumerable.Range(Convert.ToInt32(x), Convert.ToInt32(y - x)).Where(number => number % k == 0).Count());
+        }
+    }
+}
