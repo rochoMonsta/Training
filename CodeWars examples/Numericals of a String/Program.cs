8196405d434ca0426646598c3065f3fba4b8402e
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-07-03 15:17:09 +0300

    git add

---
+++ CodeWars examples/Numericals of a String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ttt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Numericals("Hello, World!"));
+            Console.ReadLine();
+        }
+        public static string Numericals(string s)
+        {
+            return String.Concat(Enumerable.Range(0, s.Length).Select(x => s.Substring(0, x).Count(y => y == s[x]) + 1));
+        }
+    }
+}
