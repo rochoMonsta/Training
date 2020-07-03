aa522b923ca373e8bd344abca65bfa1acc2d14c9
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-07-03 14:46:04 +0300

    git add

---
+++ CodeWars examples/Numericals_of_a_String/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Numericals_of_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Numericals("Hello, World! It's me, JomoPipi!") == "11121111213112111131224132411122");
+            Console.ReadLine();
+        }
+        public static string Numericals(string s)
+        {
+            string result = "";
+            var letter_dict = new Dictionary<char, int>();
+            foreach (var letter in s)
+            {
+                List<char> dict_keys = new List<char>(letter_dict.Keys);
+                if (dict_keys.Contains(letter))
+                {
+                    letter_dict[letter]++;
+                    result += letter_dict[letter];
+                }
+                else
+                {
+                    letter_dict.Add(letter, 1);
+                    result += letter_dict[letter];
+                }
+            }
+            return result;
+        }
+    }
+}
