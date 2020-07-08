9d8d10807f5937e6f3f12429a2f440f23415decf
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-07-08 18:55:04 +0300

    git add

---
+++ CodeWars examples/Password_Maker/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Password_Maker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MakePassword(16, true, true, true));
+            Console.ReadLine();
+        }
+        public static string MakePassword(int len, bool flagUpper, bool flagLower, bool flagDigit)
+        {
+            int number_or_letter = 1;
+
+            StringBuilder password = new StringBuilder("");
+            Random random = new Random(Guid.NewGuid().ToByteArray().Sum(x => x));
+
+            for (int i = 0; i < len; ++i)
+            {
+                if (flagDigit && (flagUpper || flagLower)) { number_or_letter = random.Next(1, 3); }
+                else if (!flagUpper && !flagLower) { number_or_letter = 2; }
+                switch (number_or_letter)
+                {
+                    case 1:
+                        char letter = (char)('a' + random.Next(0, 26));
+                        while (password.ToString().ToLower().Contains(letter))
+                            letter = (char)('a' + random.Next(0, 26));
+
+                        if (flagUpper == true && flagLower == true)
+                        {
+                            int lower_or_upper = random.Next(1, 3);
+                            switch (lower_or_upper)
+                            {
+                                case 1: password.Append(Char.ToUpper(letter)); break;
+                                case 2: password.Append((letter)); break;
+                            }
+                        }
+                        else if (flagUpper && !flagLower)
+                            password.Append(Char.ToUpper(letter));
+                        else
+                            password.Append(letter);
+                        break;
+                    case 2:
+                        password.Append(random.Next(0, 10));
+                        break;
+                }
+            }
+            return password.ToString();
+        }
+    }
+}
---
+++ CodeWars examples/Password_Maker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Password_Maker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Password_Maker")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e5b9c5b8-0e5b-4376-bd50-0638eec3b55f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
