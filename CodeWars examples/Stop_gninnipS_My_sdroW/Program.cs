b341bce8d35625b1fbc43e34393a24ad1d0eb810
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-07-13 21:31:42 +0300

    git add

--- CodeWars examples/Stop_gninnipS_My_sdroW/Program.cs
+++ CodeWars examples/Stop_gninnipS_My_sdroW/Program.cs
@@ -10,19 +10,19 @@ namespace Stop_gninnipS_My_sdroW
             Console.WriteLine(SpinWords("You are almost to the last test"));
             Console.ReadLine();
         }
-        public static string SpinWords(string sentence)
-        {
-            var words_array = sentence.Split();
-            for (int i = 0; i < words_array.Length; ++i)
-            {
-                if (words_array[i].Length > 4)
-                    words_array[i] = new string(words_array[i].Reverse().ToArray());
-            }
-            return String.Join(" ", words_array);
-        }
         //public static string SpinWords(string sentence)
         //{
-        //    return String.Join(" ", sentence.Split().Select(word => word.Length >= 5 ? new string(word.Reverse().ToArray()) : word));
+        //    var words_array = sentence.Split();
+        //    for (int i = 0; i < words_array.Length; ++i)
+        //    {
+        //        if (words_array[i].Length > 4)
+        //            words_array[i] = new string(words_array[i].Reverse().ToArray());
+        //    }
+        //    return String.Join(" ", words_array);
         //}
+        public static string SpinWords(string sentence)
+        {
+            return String.Join(" ", sentence.Split().Select(word => word.Length >= 5 ? new string(word.Reverse().ToArray()) : word));
+        }
     }
 }
