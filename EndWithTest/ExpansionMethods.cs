8d3d966be42c20638d5df797b27c6fe089c2c6f0
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-07-22 16:21:05 +0300

    git add

---
+++ EndWithTest/ExpansionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EndWithTest
+{
+    static class ExpansionMethods
+    {
+        // Метод розширення для String який виконує перевірку чи закінчується sentences на value
+        public static bool EndWith (this String sentences, string value)
+        {
+            for (int i = value.Length - 1, j = 1; i >= 0; --i, ++j)
+                if (sentences[sentences.Length - j] != value[i])
+                    return false;
+            return true;
+        }
+        // Метод розширення для String який виконує перевірку чи починається sentences на value
+        public static bool StartWith (this String sentences, string value)
+        {
+            for (int i = 0; i < value.Length; ++i)
+                if (sentences[i] != value[i])
+                    return false;
+            return true;
+        }
+    }
+}
---
+++ EndWithTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EndWithTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Roman".EndsWith("oman")); //стандартний метод
+            Console.WriteLine("Roman".EndWith("oman"));
+            Console.WriteLine("Roman".StartsWith("ro")); //стандартний метод
+            Console.WriteLine("Roman".StartWith("ro"));
+            Console.WriteLine("Roman".StartsWith("Ro"));
+            Console.WriteLine("Roman".StartWith("Ro"));
+            Console.ReadLine();
+        }
+    }
+}
