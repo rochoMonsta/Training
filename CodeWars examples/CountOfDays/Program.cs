006212b2301e9d96bba6c2ac8e1dbd305b0b93b6
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-07-25 21:30:39 +0300

    git add

---
+++ CodeWars examples/CountOfDays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace CountOfDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UnluckyDays(1986));
+            Console.ReadLine();
+        }
+        public static int UnluckyDays(int year)
+        {
+            return Enumerable.Range(1, 12).Select(x => new DateTime(year, x, 13)).Count(x => x.DayOfWeek == DayOfWeek.Friday);
+        }
+        public static int UnluckyDays(int year, DayOfWeek dayOfWeek)
+        {
+            return Enumerable.Range(1, 12).Select(x => new DateTime(year, x, 13)).Count(x => x.DayOfWeek == dayOfWeek);
+        }
+    }
+}
